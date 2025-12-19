4f04762fa43c64c6058b87e57871ccb2def92f2b
ryom <ryouma7172z@gmail.com>
2025-12-19 21:51:14 +0900

    Adds Lorenz model and Runge-Kutta method

--- main.py
+++ main.py
@@ -1,6 +1,46 @@
-def main():
-    print("Hello from lorenz-sparse!")
+import matplotlib.pyplot as plt
+import numpy as np
 
+N = 1000 # number of loops
+t_n = 20 # end time
+h = t_n / N # inverval
+
+dim = 3 # dimension
+x_init = np.array([1.0, 1.0, 1.0])
+
+# Runge-Kutta method
+def RungeKutta(x, f):
+    k1 = f(x)
+    k2 = f(x + h * k1 / 2.0)
+    k3 = f(x + h * k2 / 2.0)
+    k4 = f(x + h * k3)
+
+    return x + h * (k1 + 2 * k2 + 2 * k3 + k4) / 6.0
+
+class LorenzModel:
+    def __init__(self, a, b, r):
+        self.a = a
+        self.b = b
+        self.r = r
+
+    def __call__(self, x):
+        dx_dt = -self.a * x[0] + self.a * x[1]
+        dy_dt = (self.r - x[2]) * x[0] - x[1]
+        dz_dt = x[0] * x[1] - self.b * x[2]
+
+        return np.array([dx_dt, dy_dt, dz_dt])
+
+def main():
+    data = np.zeros((N + 1, dim))
+    model = LorenzModel(10, 8 / 3, 28)
+
+    data[0] = x_init.copy()
+    for i in range(N):
+        data[i + 1] = RungeKutta(data[i], model)
+
+    ax = plt.figure().add_subplot(projection='3d')
+    ax.plot(data[:, 0], data[:, 1], data[:, 2])
+    plt.show()
 
 if __name__ == "__main__":
     main()
