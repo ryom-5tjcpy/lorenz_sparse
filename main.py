dd476feb19dd864fdbcb8945b1b0ecd38b27b43b
ryom <ryouma7172z@gmail.com>
2025-12-20 22:33:11 +0900

    Added a machine learning using LASSO
    Using scikit-leaning library

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.linear_model import Lasso
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
 
-N = 1000 # number of loops
+N = 2000 # number of loops
 t_n = 20 # end time
 h = t_n / N # inverval
 
@@ -40,6 +43,24 @@ def main():
 
     ax = plt.figure().add_subplot(projection='3d')
     ax.plot(data[:, 0], data[:, 1], data[:, 2])
+    plt.show()
+
+    shift = 1
+
+    x = data[0:(N + 1 - shift)]
+    y = data[shift:N + 1]
+
+    lasso = Lasso(alpha=0.01, max_iter=1000)
+    lasso.fit(x, y)
+
+    y_pred = lasso.predict(x)
+    mse = mean_squared_error(y, y_pred)
+
+    print(mse)
+
+    ax = plt.figure().add_subplot(projection='3d')
+    ax.plot(data[:, 0], data[:, 1], data[:, 2])
+    ax.plot(y_pred[:, 0], y_pred[:, 1], y_pred[:, 2])
     plt.show()
 
 if __name__ == "__main__":
